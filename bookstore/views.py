eb611e1e822c92f150073f7daa1c4cecc38b16ff
LuscasV <luscas.pessoal19@gmail.com>
2026-01-06 20:15:23 -0300

    feat: deploy automatico com github actions

---
+++ bookstore/views.py
@@ -0,0 +1,35 @@
+from django.http import HttpResponse, JsonResponse
+from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
+import git
+
+# def home(request):
+#     return JsonResponse({
+#         "status": "ok",
+#         "message": "Bookstore API is running 🚀",
+#         "endpoints": {
+#             "products": "/bookstore/v1/product",
+#             "order": "/bookstore/v1/order",
+#         }
+#     })
+
+@csrf_exempt
+def update(request):
+    if request.method == "POST":
+        '''
+        pass the path of the diectory where your project will be
+        stored on PythonAnywhere in the git.Repo() as parameter.
+        Here the name of my directory is "test.pythonanywhere.com"
+        '''
+        repo = git.Repo('/home/drsantos20/bookstore')
+        origin = repo.remotes.origin
+
+        origin.pull()
+        return HttpResponse("Updated code on PythonAnywhere")
+    else:
+        return HttpResponse("Couldn't update the code on PythonAnywhere")
+
+
+def hello_world(request):
+    template = loader.get_template('hello_world.html')
+    return HttpResponse(template.render())
